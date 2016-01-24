75048a304c42f282c2c9449c8cd2dae9f7525071
erpy <oj.bae@erpyui-MacBook-Pro.local>
2016-01-24 15:12:59 +0900

    update

--- src/main/java/com/erpy/boardwang/board/BoardCP.java
+++
@@ -1,8 +0,0 @@
-package com.erpy.boardwang.board;
-
-/**
- * Created by oj.bae on 2016. 1. 17..
- */
-public interface BoardCP {
-
-}
---
+++ src/main/java/com/erpy/boardwang/main/CrawlContent.java
@@ -0,0 +1,7 @@
+package com.erpy.boardwang.main;
+
+/**
+ * Created by oj.bae on 2016. 1. 24..
+ */
+public class CrawlContent {
+}
